c12904be67760572e7bb73ac117bc64d32369d58
Diego <diegohenriqueptc@hotmail.com>
2022-10-08 13:00:11 -0300

    entrega

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,1 +1,83 @@
 /* Desenvolva sua lógica aqui ... */
+const listTypesMusic = document.querySelector(".listTypes")
+
+function renderCategories (types){
+
+    types.forEach(type => {
+        let li = document.createElement("li")
+        let button = document.createElement("button")
+        button.className = "btn-categories"
+        button.innerText = `${type}`
+        li.className ="list-style"
+        li.appendChild(button)
+        listTypesMusic.appendChild(li)
+        
+    });
+
+}
+renderCategories(categories)
+
+
+let cards = document.querySelector(".cards")
+
+function renderCard(listProduts){
+
+    listProduts.forEach((product) => {
+        let li = document.createElement("li")
+        let img = document.createElement("img")
+        let divTitle = document.createElement("div")
+        let spanBand = document.createElement("span")
+        let spanYaer = document.createElement("span")
+        let album = document.createElement("p")
+        let price = document.createElement("span")
+        let divPrice =document.createElement("div")
+        let btnBuy = document.createElement("button")
+        
+        li.className ="list-style"
+        img.src = product.img
+        img.alt = `Álbum ${product.title}`
+        divTitle.className = "band-year"
+        spanBand.innerText = product.band
+        spanYaer.innerText = product.year
+        album.innerText = product.title
+        price.innerText = `R$ ${product.price},00`
+        divPrice.className= "btn-price"
+        btnBuy.innerText= "Comprar"
+        btnBuy.className ="btn-categories"
+
+        cards.append(li)
+        divTitle.append(spanBand, spanYaer)
+        divPrice.append(price, btnBuy)
+        li.append(img, divTitle, album, divPrice)
+
+    })
+
+}
+renderCard(products)
+
+function renderAll(){
+    const btnCategories = document.querySelectorAll(".btn-categories")
+    let cards = document.querySelector(".cards")
+    
+    btnCategories.forEach((btn) => {
+        btn.addEventListener("click", function(){
+
+            cards.innerHTML = ""
+            const filter = btn.innerText
+
+            if(filter == "Todos"){
+                renderCard(products)
+            }
+
+            const genreFiltered = filterGenre(filter)
+
+            renderCard(genreFiltered)
+        })
+    })
+}
+function filterGenre(text){
+    const genreList = products.filter((product)=> product.genre.includes(text))
+    return genreList
+}
+renderAll()
+
--- scripts/theme.js
+++ scripts/theme.js
@@ -1,1 +1,25 @@
 /* Desenvolva sua lógica aqui ... */
+const buttonTheme = document.querySelector("#btn-theme")
+const imgBtn = document.querySelector("#img-btn")
+const body = document.body
+const theme = "dark-mode"
+const srcImg = ["./assets/img/moon.png", "./assets/img/sun.png"]
+let darkMode
+
+imgBtn.src = srcImg[0]
+
+buttonTheme.addEventListener("click", function(){
+
+        darkMode = !darkMode
+        body.classList.toggle(theme)
+
+        localStorage.setItem(theme, darkMode)
+        
+        if(darkMode){
+            imgBtn.src = srcImg[1]
+            
+        }else{
+          imgBtn.src = srcImg[0]
+          localStorage.removeItem
+        }
+})
